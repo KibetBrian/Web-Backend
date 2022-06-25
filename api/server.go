2e4743eccebd327714ef8aec319df174d5237cb6
KibetBrian <briankibet2040@gmail.com>
2022-06-25 13:46:19 +0300

    adds news server

---
+++ api/server.go
@@ -0,0 +1,13 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+
+func NewServer() *Server{
+	server := &Server{db: DBQueries(), router: gin.Default()}	
+
+	server.router.POST("/user/create", server.CreateUser)
+	return server;
+}
