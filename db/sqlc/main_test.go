08550a3c309a0799cb4c535becba79aecccb21c5
KibetBrian <brinakibet2040@gmail.com>
2022-07-05 23:29:23 +0300

    adds env

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -2,20 +2,21 @@ package db
 
 import (
 	"database/sql"
-	_ "github.com/lib/pq"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/KibetBrian/backend/utils"
+	_ "github.com/lib/pq"
 )
 var testQueries *Queries
 
-const (
-	dbDriver="postgres"
-	dbSource="postgresql://brian:brian@127.0.0.1:5432/election?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver,dbSource);
+	configs, err := utils.LoadConfig("../../")
+	if err != nil{
+		log.Fatalf("Failed to load config. Error: %v", err)
+	}
+	conn, err := sql.Open(configs.DbDriver,configs.DbSource);
 	if err != nil{
 		log.Fatalf("\n Database connection failed: %v", err)
 	}
