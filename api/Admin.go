29c9e18b8255f5900882e243b53c076dcf76d392
KibetBrian <brinakibet2040@gmail.com>
2022-07-11 23:41:31 +0300

    handles candidade registration

--- api/Admin.go
+++ api/Admin.go
@@ -14,12 +14,21 @@ import (
 )
 
 type VoterRegistrationRequest struct {
-
 	UserId uuid.UUID `json:"userId"`
 	VotersName string `json:"votersName"`
 	VotersEmail string `json:"votersEmail"`
 	VotersAddress string `json:"votersAddress"`
+}
 
+type ContestantRegistrationRequest struct {
+	FullName string `json:"fullName" binding:"required"`
+	Email string `json:"email" binding:"required"`
+	Position string `json:"position" binding:"required"`
+	Region string `json:"region" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	ImageAddress string `json:"imageAddress" binding:"required"`
+	EthereumAddress string `json:"ethereumAddress" binding:"required"`
+	NationalIdNumber int64 `json:"nationalIdNumber" binding:"required"`	
 }
 
 type VoterChainParams struct {
@@ -45,7 +54,6 @@ func (s *Server) RegisterVoter(c  *gin.Context) {
 		c.JSON(http.StatusForbidden, "UserId doesn't match with email provided")
 		return
 	}	
-
 	arg := db.RegisterVoterParams{
 		 FullName: req.VotersName,
 		 Email: req.VotersEmail,
@@ -112,4 +120,60 @@ func(s *Server) GetChainVoters(c *gin.Context){
 		return
 	}
 	c.JSON(http.StatusOK, jsonData)
+}
+
+func (s *Server) RegisterContestant(c *gin.Context){
+	var req ContestantRegistrationRequest
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, utils.ErrResponse("Invalid parameters. Error: ", err))
+		return
+	}
+
+	//Make a post request to the blockchain client
+	const url = "http://127.0.0.1:8000/candidate/add"
+	const method = "POST"
+	const contentType = "application/json"
+
+	values := map[string]string{
+		"admin": "brian",
+		"candidatesName": req.FullName,
+		"candidatesAddress": req.EthereumAddress,
+		"position": req.Position,
+	}
+	jsonData, err := json.Marshal(values)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,"Failed to marshal values")
+		return
+	}
+	res, err := http.Post(url, contentType, bytes.NewBuffer(jsonData))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError,"Failed to make a post request to blockchain client")
+		return
+	}
+	var jsonRes map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&jsonRes)
+
+	if jsonRes["success"] == false {
+		c.JSON(http.StatusConflict, jsonRes)
+		return
+	}
+	//Save data to the sql database
+	arg := db.RegisterContestantParams{
+		FullName: req.FullName,
+		Email: req.Email,
+		Position: req.Position,
+		Description: req.Description,
+		Region: req.Region,
+		EthereumAddress: req.EthereumAddress,
+		NationalIDNumber: req.NationalIdNumber,
+		ImageAddress: req.ImageAddress,
+	}
+	ctx := context.Background();
+	contestant, err := s.db.RegisterContestant(ctx, arg)
+	if err!=nil {
+		c.JSON(http.StatusInternalServerError,gin.H{"message": "Failed to register contestant", "error": err})
+		return
+	}
+	c.JSON(http.StatusOK,gin.H{"message:":"Contestant registered","contestant":contestant})
 }
