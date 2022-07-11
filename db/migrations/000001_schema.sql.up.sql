9d4e9865177c618a1a34e237f29f20fbd89044cd
KibetBrian <brinakibet2040@gmail.com>
2022-07-11 23:40:57 +0300

    more fields

--- db/migrations/000001_schema.sql.up.sql
+++ db/migrations/000001_schema.sql.up.sql
@@ -9,20 +9,22 @@ CREATE TABLE "users" (
   "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
   "full_name" varchar NOT NULL,
   "email" varchar UNIQUE NOT NULL,
-  "password" varchar NOT NULL
+  "password" varchar NOT NULL,
+  "is_admin" boolean DEFAULT 'FALSE'
 );
 
 CREATE TABLE "voters" (
-  "id" uuid NOT NULL DEFAULT  uuid_generate_v4(),
+  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
   "full_name" varchar NOT NULL,
   "email" varchar NOT NULL,
   "registered_at" timestamptz PRIMARY KEY NOT NULL DEFAULT 'now()',
-  "voted_at" timestamptz DEFAULT 'now()',
+  "voted_at" timestamptz,
+  "voted" boolean,
   "voters_public_address" varchar NOT NULL
 );
 
 CREATE TABLE "admins" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
   "full_name" varchar NOT NULL,
   "email" varchar NOT NULL,
   "password" varchar NOT NULL,
@@ -30,13 +32,16 @@ CREATE TABLE "admins" (
 );
 
 CREATE TABLE "contestants" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
+  "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
   "full_name" varchar NOT NULL,
   "email" varchar NOT NULL,
-  "password" varchar NOT NULL,
   "position" varchar NOT NULL,
   "registered_at" timestamptz DEFAULT 'now()',
-  "description" varchar NOT NULL
+  "description" varchar NOT NULL,
+  "region" varchar NOT NULL,
+  "ethereum_address" varchar NOT NULL,
+  "national_id_number" bigint NOT NULL,
+  "image_address" varchar NOT NULL
 );
 
 CREATE INDEX ON "admins" ("full_name");
--- db/queries/contestants.sql
+++ db/queries/contestants.sql
@@ -1,3 +1,6 @@
 -- name: RegisterContestant :one
-INSERT INTO contestants(full_name,email,password,position, description)
-VALUES ($1,$2,$3,$4, $5) RETURNING *;
+INSERT INTO contestants(full_name,email,position, description, region,ethereum_address,national_id_number, image_address)
+VALUES ($1,$2,$3,$4,$5, $6,$7,$8) RETURNING *;
+
+-- name: GetAllCandidates :many
+SELECT * FROM contestants;
