7e559fef95fa60341a96a9975aa3f5c6a3216194
KibetBrian <briankibet2040@gmail.com>
2022-08-12 00:01:23 +0300

    update user handler

--- api/User.go
+++ api/User.go
@@ -22,18 +22,27 @@ type LoginRequest struct {
 	Password string `json:"password" binding:"required"`
 }
 
+type UpdateVotedPresidentReq struct {
+	Email    string `json:"email" binding:"required"`
+}
+
+type UpdateVotedGovernorReq struct {
+	Email    string `json:"email" binding:"required"`
+}
+
+
 type GetUserRequest struct {
 	Id uuid.UUID `json:"id" uri:"email" binding:"required"`
 }
 
 type LoginResponse struct {
-	ID       uuid.UUID `json:"id"`
-	FullName string    `json:"fullName"`
-	Email    string    `json:"email"`
-	IsAdmin  bool      `json:"isAdmin"`
-	VotedPresident bool `json:"votedPresident"`
-	VotedGovernor bool `json:"votedGovernor"`
-	RegisteredVoter bool `json:"registeredVoter"`
+	ID              uuid.UUID `json:"id"`
+	FullName        string    `json:"fullName"`
+	Email           string    `json:"email"`
+	IsAdmin         bool      `json:"isAdmin"`
+	VotedPresident  bool      `json:"votedPresident"`
+	VotedGovernor   bool      `json:"votedGovernor"`
+	RegisteredVoter bool      `json:"registeredVoter"`
 }
 
 func (s *Server) RegisterUser(ctx *gin.Context) {
@@ -122,13 +131,13 @@ func (s *Server) Login(c *gin.Context) {
 		return
 	}
 	res := &LoginResponse{
-		ID:       user.ID,
-		FullName: user.FullName,
-		Email:    user.Email,
-		IsAdmin: user.IsAdmin.Bool,
+		ID:              user.ID,
+		FullName:        user.FullName,
+		Email:           user.Email,
+		IsAdmin:         user.IsAdmin.Bool,
 		RegisteredVoter: user.RegisteredVoter.Bool,
-		VotedPresident: user.VotedPresident.Bool,
-		VotedGovernor: user.VotedGovernor.Bool,
+		VotedPresident:  user.VotedPresident.Bool,
+		VotedGovernor:   user.VotedGovernor.Bool,
 	}
 	c.JSON(http.StatusOK, res)
 }
@@ -143,4 +152,58 @@ func (s *Server) GetTotalUsersNum(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, num)
+}
+
+
+func (s *Server) UpdateVotedPresident(c *gin.Context) {
+	ctx := context.Background();
+	var req UpdateVotedPresidentReq
+	err := c.ShouldBindJSON(&req)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, "Bad request")
+	}
+	user, err := s.db.UpdateVotedPresident(ctx, req.Email)
+	if err != nil{
+		if err==sql.ErrNoRows{
+			c.JSON(http.StatusNotFound, "User not found")
+		}
+		c.JSON(http.StatusInternalServerError, "Internal Server Error")
+	}
+	res := &LoginResponse{
+		ID:              user.ID,
+		FullName:        user.FullName,
+		Email:           user.Email,
+		IsAdmin:         user.IsAdmin.Bool,
+		RegisteredVoter: user.RegisteredVoter.Bool,
+		VotedPresident:  user.VotedPresident.Bool,
+		VotedGovernor:   user.VotedGovernor.Bool,
+	}
+	c.JSON(http.StatusOK, res)
+}
+
+func (s *Server) UpdateVotedGovernor(c *gin.Context) {
+	ctx := context.Background();
+	var req UpdateVotedGovernorReq
+	err := c.ShouldBindJSON(&req)
+	if err != nil{
+		c.JSON(http.StatusBadRequest, "Bad request")
+	}
+	user, err := s.db.UpdateVotedGovernor(ctx, req.Email)
+	if err != nil{
+		if err==sql.ErrNoRows{
+			c.JSON(http.StatusNotFound, "User not found")
+		}
+		c.JSON(http.StatusInternalServerError, "Internal Server Error")
+	}
+
+	res := &LoginResponse{
+		ID:              user.ID,
+		FullName:        user.FullName,
+		Email:           user.Email,
+		IsAdmin:         user.IsAdmin.Bool,
+		RegisteredVoter: user.RegisteredVoter.Bool,
+		VotedPresident:  user.VotedPresident.Bool,
+		VotedGovernor:   user.VotedGovernor.Bool,
+	}
+	c.JSON(http.StatusOK, res)
 }
